4aef286bf2edb5462cbf6ea59645a6a28985c66e
neuecc <ils@neue.cc>
2025-03-31 03:13:00 +0900

    c

--- tests/ZLinq.Tests/Linq/ConcatTest.cs
+++ tests/ZLinq.Tests/Linq/ConcatTest.cs
@@ -264,17 +264,17 @@ public class ConcatTest
 
         // We need to use checked arithmetic summing up the collections' counts.
         Assert.Throws<OverflowException>(() => supposedlyLargeCollection.AsValueEnumerable().Concat(tinyCollection).Count());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(supposedlyLargeCollection).Count());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).Count());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(supposedlyLargeCollection).Count());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).Count());
 
         //// This applies to ToArray() and ToList() as well, which try to preallocate the exact size
         //// needed if all inputs are ICollections.
-        Assert.Throws<OverflowException>(() => supposedlyLargeCollection.Concat(tinyCollection).ToArray());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(supposedlyLargeCollection).ToArray());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).ToArray());
+        Assert.Throws<OverflowException>(() => supposedlyLargeCollection.AsValueEnumerable().Concat(tinyCollection).ToArray());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(supposedlyLargeCollection).ToArray());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).ToArray());
 
-        Assert.Throws<OverflowException>(() => supposedlyLargeCollection.Concat(tinyCollection).ToList());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(supposedlyLargeCollection).ToList());
-        Assert.Throws<OverflowException>(() => tinyCollection.Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).ToList());
+        Assert.Throws<OverflowException>(() => supposedlyLargeCollection.AsValueEnumerable().Concat(tinyCollection).ToList());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(supposedlyLargeCollection).ToList());
+        Assert.Throws<OverflowException>(() => tinyCollection.AsValueEnumerable().Concat(tinyCollection).Concat(tinyCollection).Concat(supposedlyLargeCollection).ToList());
     }
 }
