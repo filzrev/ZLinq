bfb1b439f4c1416c62e2713af051684f44fbf308
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2025-04-13 10:42:55 +0900

    perf: Shuffle followed by Take/Skip/TakeLast/SkipLast

--- sandbox/Benchmark/Benchmarks/ShuffleBench.cs
+++ sandbox/Benchmark/Benchmarks/ShuffleBench.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using System.Runtime.InteropServices;
+using ZLinq;
 
-namespace Benchmark
+namespace Benchmark;
+
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+public class ShuffleBench
 {
-    class ShuffleBench
+    [Params(/*10, 100,*/ 1000, /*10000,*/ 100000)]
+    public int N;
+
+    [Params(10, 100, 1000, 100000)]
+    public int M;
+
+    int[] array = default!;
+
+    [GlobalSetup]
+    public void Setup()
     {
+        var rand = new Random(42);
+        array = new int[N];
+        rand.NextBytes(MemoryMarshal.Cast<int, byte>(array));
     }
+
+#if NET10_0_OR_GREATER
+
+    [Benchmark]
+    [BenchmarkCategory(Categories.LINQ)]
+    public int ShuffleTakeLinq()
+    {
+        var src = array.Shuffle().Take(M);
+        int i = 0;
+        using var e = src.GetEnumerator();
+        while (e.MoveNext())
+        {
+            i++;
+            _ = e.Current;
+        }
+        return i;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory(Categories.ZLinq)]
+    public int ShuffleTakeZL()
+    {
+        var src = array.AsValueEnumerable().Shuffle().Take(M);
+        int i = 0;
+        using var e = src.Enumerator;
+        while (e.TryGetNext(out var item))
+        {
+            i++;
+        }
+        return i;
+    }
+
+#endif
 }
--- src/ZLinq/Internal/RandomShared.cs
+++ src/ZLinq/Internal/RandomShared.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace ZLinq.Internal;
 
@@ -22,6 +19,24 @@ internal static class RandomShared
         Random.Shared.Shuffle(span);
 #else
         Shared.Value.Shuffle(span);
+#endif
+    }
+
+    public static void Shuffle<T>(T[] array, int count)
+    {
+#if NET8_0_OR_GREATER
+        Random.Shared.Shuffle(array.AsSpan(), count);
+#else
+        Shared.Value.Shuffle(array.AsSpan(), count);
+#endif
+    }
+
+    public static void Shuffle<T>(Span<T> span, int count)
+    {
+#if NET8_0_OR_GREATER
+        Random.Shared.Shuffle(span, count);
+#else
+        Shared.Value.Shuffle(span, count);
 #endif
     }
 
@@ -56,4 +71,33 @@ internal static class RandomShared
     }
 
 #endif
+
+    static void Shuffle<T>(this Random random, Span<T> values, int count)
+    {
+        if (count <= 0)
+            return;
+
+        int length = values.Length;
+        if (count > length)
+            count = length;
+
+        int n = length;
+        if (n > count)
+            n = count;
+
+        if (n == length)
+            n--;  // exclude last item
+
+        for (int i = 0; i < n; i++)
+        {
+            int j = random.Next(i, length);
+
+            if (j != i)
+            {
+                T temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+            }
+        }
+    }
 }
